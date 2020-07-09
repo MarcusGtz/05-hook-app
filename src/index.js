33a279a72bff5eaae84c291bccd19f892620082d
marcos <skywere@gmail.com>
2020-07-08 22:32:25 -0500

    Video 107

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import { HooksApp } from "./HooksApp";
 // import { CounteApp } from "./components/01-useState/CounteApp";
-import { CounterWithCustomHoos } from "./components/01-useState/CounterWithCustomHoos";
+// import { CounterWithCustomHoos } from "./components/01-useState/CounterWithCustomHoos";
+import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 
-ReactDOM.render(<CounterWithCustomHoos />, document.getElementById("root"));
+ReactDOM.render(<SimpleForm />, document.getElementById("root"));
