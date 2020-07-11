19e6e0c6edfd40122c3ae1a4c2ccf7d726889c52
marcos <skywere@gmail.com>
2020-07-10 23:44:17 -0500

    V 120, Secci 10 Terminado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Padre } from "./components/07-tarea-memo/Padre";
 // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
 // import { Layout } from "./components/05-useLayoutEffect/Layout";
 // import { Memorieze } from "./components/06-memos/Memorieze";
 // import { MemoHook } from "./components/06-memos/MemoHook";
-import { CallbackHook } from "./components/06-memos/CallbackHook";
+// import { CallbackHook } from "./components/06-memos/CallbackHook";
 // import { FocusScreen } from "./components/04-useRef/FocusScreen";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
@@ -13,4 +14,4 @@ import { CallbackHook } from "./components/06-memos/CallbackHook";
 // import { CounterWithCustomHoos } from "./components/01-useState/CounterWithCustomHoos";
 // import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 
-ReactDOM.render(<CallbackHook />, document.getElementById("root"));
+ReactDOM.render(<Padre />, document.getElementById("root"));
